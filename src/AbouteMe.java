ca51a0cb8e010bc6b302e56762f4cc6af8be3467
bellawilson <wilsonbella880@gmail.com>
2023-08-28 18:42:44 -0300

    feat: Classe AbouteMe

--- src/AbouteMe.java
+++ src/AbouteMe.java
@@ -1,5 +1,29 @@
-public class Main {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AbouteMe {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome : ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome :");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade :");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite o seu peso :");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Digite sua altura :");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Meu peso é " + peso + " Quilos");
+        System.out.println("Minha altura é " + altura + " cm" );
+
     }
 }
